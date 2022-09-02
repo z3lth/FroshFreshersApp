300c119831584a5ac0bfc8c1100d9e28444366af
shreyas1504 <112496017+shreyas1504@users.noreply.github.com>
2022-09-02 13:41:57 +0530

    Add files via upload

---
+++ lib/SocietyPages/Edc_page.dart
@@ -0,0 +1,275 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:get/get.dart';
+import 'package:meditation/FrostedGlass1.dart';
+import 'package:meditation/detail_page.dart';
+import 'package:meditation/discover_page.dart';
+import 'package:meditation/gallery.dart';
+import 'package:meditation/screens/home.dart';
+import 'package:meditation/widgets/category_boxes.dart';
+import 'package:meditation/icons.dart';
+import 'package:meditation/widgets/discover_card3.dart';
+import 'package:meditation/widgets/svg_asset.dart';
+import 'package:url_launcher/url_launcher.dart';
+import '../../frostedglass.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class EdcPage extends StatelessWidget {
+  const EdcPage({Key? key}) : super(key: key);
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       debugShowCheckedModeBanner: false,
+//       title: 'Flutter Demo',
+//       theme: ThemeData(
+//         primarySwatch: Colors.blue,
+//       ),
+//       home: const MyHomePage(),
+//     );
+//   }
+// }
+//
+// class MyHomePage extends StatelessWidget {
+//   const MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: Text('EDC',
+          textAlign:TextAlign.center,
+          style: GoogleFonts.poppins(
+            textStyle: TextStyle(color: Colors.white, letterSpacing: .5,
+                fontWeight: FontWeight.w700),
+          ),
+        ),
+        backgroundColor: Color(0xff1C2031),
+        shadowColor: Colors.black,
+
+      ) ,
+      body: Container(
+
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+            // Import image in pubspec.yaml first
+            image: AssetImage('assets/pics/bgallsoc.jpg'),
+            // BoxFit.cover scales up the image until it completely covers the screen
+            fit: BoxFit.cover,
+          ),
+        ),
+        // alignment is for centering the frostedglass, we don't use Center();
+        //  because if you want to change its position and remove the Center();
+        //  widget, everything will get messy the script crashes.
+        alignment: Alignment.center,
+
+        child:Column(
+
+
+          children:
+
+          [ SizedBox(height: 20.h),Padding(
+            padding: const EdgeInsets.all(10.0),
+            child: FrostedGlassBox(
+              // theWidth is the width of the frostedglass
+                theWidth: 350.0,
+                // theHeight is the height of the frostedglass
+                theHeight: 600.0,
+                // theChild is the child of the frostedglass
+
+
+                theChild: Padding(
+                  padding: const EdgeInsets.all(15.0),
+                  child: Column(
+
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children:[
+                        Image.asset('assets/pics/EDC.png',
+                            width: 300,
+                            height: 200,
+                            fit:BoxFit.fill
+                        ),
+                        SizedBox(height: 10.h),
+
+                        // Text(
+                        //   'About Us                                                                                           ',
+                        //   //'FROSH comprises of various events for the fresher class of Thapar Institute of Engineering and Technology to ease their new surroundings.                                                                  '
+                        //   //  'instagram.com/froshtiet/                                 '
+                        //   //   'linkedin.com/company/frosh-tiet                           '
+                        //   //   'facebook.com/froshtiet         ',
+                        //   style: TextStyle(color: Colors.white54, fontSize: 30.0,fontFamily: 'Roboto',fontWeight: FontWeight.bold,), textAlign: TextAlign.center,)
+
+                        Text('Entrepreneurship Development Cell is a non-profit organization built on idea of "Ideate, Ascend, Lead." EDC promotes entrepreneurship among young minds, values unconventional thinking, and assists individuals in becoming entrepreneurial leaders.',
+
+                          style: GoogleFonts.poppins(
+                            textStyle: TextStyle(color: Colors.white, letterSpacing: .5,fontSize: 15.0,),
+                          ),
+                        ),
+                        Text('',
+
+
+                          style: TextStyle(color: Colors.white, fontSize: 20.0,), textAlign: TextAlign.center,),
+
+                        SizedBox(height: 10.h),
+                        Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              // InkWell(
+                              //     onTap: () async{
+                              //       final Uri _url = Uri.parse('https://www.instagram.com/froshtiet/');
+                              //
+                              //       if (!await launchUrl(_url)) {
+                              //         throw 'Could not launch $_url';
+                              //       }
+                              //     },
+                              //   child:
+                              Image.asset('assets/pics/instagram.png',
+                                width: 35,
+                                height: 35,
+                              ),
+                              TextButton(onPressed: ()async{
+                                final Uri _url = Uri.parse('https://www.instagram.com/edc_tiet/');
+
+                                if (!await launchUrl(_url)) {
+                                  throw 'Could not launch $_url';
+                                }
+                              }, child:
+                              Text('/edctiet',
+                                style: GoogleFonts.poppins(
+                                  textStyle: TextStyle(color: Colors.white54, letterSpacing: .5,fontSize: 20.0,),
+                                ),
+                              ),
+                              )]),
+
+                        Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              //
+                              Image.asset('assets/pics/web.png',
+                                width: 35,
+                                height: 35,
+                              ),
+                              TextButton(onPressed: ()async{
+                                final Uri _url = Uri.parse('https://sites.google.com/thapar.edu/edctiet/home?authuser=0');
+
+                                if (!await launchUrl(_url)) {
+                                  throw 'Could not launch $_url';
+                                }
+                              }, child:
+                              Text('/edctiet',
+                                style: GoogleFonts.poppins(
+                                  textStyle: TextStyle(color: Colors.white54, letterSpacing: .5,fontSize: 20.0,),
+                                ),
+                              ),
+                              )
+                            ]
+                        ),
+
+                        // Row(
+                        //     mainAxisAlignment: MainAxisAlignment.center,
+                        //     children: [
+                        //       //
+                        //       Image.asset('assets/pics/linkedin.png',
+                        //         width: 35,
+                        //         height: 35,
+                        //       ),
+                        //       TextButton(onPressed: ()async{
+                        //         final Uri _url = Uri.parse('https://www.linkedin.com/company/frosh-tiet');
+                        //
+                        //         if (!await launchUrl(_url)) {
+                        //           throw 'Could not launch $_url';
+                        //         }
+                        //       }, child:
+                        //       Text('/froshtiet',style: TextStyle(color: Colors.white54, fontSize: 25.0,), textAlign: TextAlign.center, ),)]),
+
+                        SizedBox(height: 10.h),
+
+
+
+                        //SizedBox(width: 350.w,),
+                        SizedBox(height: 10.h),
+                        // DiscoverCard3(
+                        //
+                        //   onTap: (){
+                        //     Navigator.push(context, MaterialPageRoute(builder: (context)=>GalleryPage()));
+                        //
+                        //   },
+                        //   title: "Our Team",
+                        //
+                        //   //subtitle: "How to read timetable?",
+                        //   // gradientStartColor: Color(0xffFC67A7),
+                        //   //gradientEndColor: Color(0xffF6815B),
+                        // ),
+
+                      ]
+                  ),
+                )
+            ),
+          ),
+            // Divider(
+            //   color: Colors.white,
+            //   thickness: 3,
+            //
+            //
+            // ),
+            Row(
+              children: [
+                // Row(
+                //     mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                //     children: [
+                //       //
+                //       Image.asset('assets/pics/instagram.png',
+                //         width: 30,
+                //         height: 35,
+                //       ),
+                //       TextButton(onPressed: ()async{
+                //         final Uri _url = Uri.parse('https://www.instagram.com/backslashtiet/');
+                //
+                //         if (!await launchUrl(_url)) {
+                //           throw 'Could not launch $_url';
+                //         }
+                //       }, child:
+                //       Text('/backslashtiet',style: TextStyle(color: Colors.white54, fontSize: 23.0,), textAlign: TextAlign.justify, ),)]),
+
+                // Spacer(),
+
+                //
+                // Row(
+                //     mainAxisAlignment: MainAxisAlignment.end,
+                //     children: [
+                //       //
+                //       Image.asset('assets/pics/web.png',
+                //         width: 30,
+                //         height: 35,
+                //       ),
+                //       TextButton(onPressed: ()async{
+                //         final Uri _url = Uri.parse('http://backslashdb.in/');
+                //
+                //         if (!await launchUrl(_url)) {
+                //           throw 'Could not launch $_url';
+                //         }
+                //       }, child:
+                //       Text('/backslashiet',style: TextStyle(color: Colors.white54, fontSize: 23.0,), textAlign: TextAlign.start, ),)]),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+
+  }
+
+  void onSeeAllTapped() {
+  }
+
+  void onSleepMeditationTapped() {
+    Get.to(() => DetailPage(), transition: Transition.rightToLeft);
+  }
+  void onDepressionHealingTapped() {
+  }
+
+  void onSearchIconTapped() {
+  }
+}
---
+++ lib/SocietyPages/Rotoract_Page.dart
@@ -0,0 +1,276 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:get/get.dart';
+import 'package:meditation/FrostedGlass1.dart';
+import 'package:meditation/detail_page.dart';
+import 'package:meditation/discover_page.dart';
+import 'package:meditation/gallery.dart';
+import 'package:meditation/screens/home.dart';
+import 'package:meditation/widgets/category_boxes.dart';
+import 'package:meditation/icons.dart';
+import 'package:meditation/widgets/discover_card3.dart';
+import 'package:meditation/widgets/svg_asset.dart';
+import 'package:url_launcher/url_launcher.dart';
+import '../../frostedglass.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class RotoractPage extends StatelessWidget {
+  const RotoractPage({Key? key}) : super(key: key);
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       debugShowCheckedModeBanner: false,
+//       title: 'Flutter Demo',
+//       theme: ThemeData(
+//         primarySwatch: Colors.blue,
+//       ),
+//       home: const MyHomePage(),
+//     );
+//   }
+// }
+//
+// class MyHomePage extends StatelessWidget {
+//   const MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: Text('Rotaract Club',
+          textAlign:TextAlign.center,
+          style: GoogleFonts.poppins(
+            textStyle: TextStyle(color: Colors.white, letterSpacing: .5,
+                fontWeight: FontWeight.w700),
+          ),
+        ),
+        backgroundColor: Color(0xff1C2031),
+        shadowColor: Colors.black,
+
+      ) ,
+      body: Container(
+
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+            // Import image in pubspec.yaml first
+            image: AssetImage('assets/pics/bgallsoc.jpg'),
+            // BoxFit.cover scales up the image until it completely covers the screen
+            fit: BoxFit.cover,
+          ),
+        ),
+        // alignment is for centering the frostedglass, we don't use Center();
+        //  because if you want to change its position and remove the Center();
+        //  widget, everything will get messy the script crashes.
+        alignment: Alignment.center,
+
+        child:Column(
+
+
+          children:
+
+          [ SizedBox(height: 20.h),Padding(
+            padding: const EdgeInsets.all(10.0),
+            child: FrostedGlassBox(
+              // theWidth is the width of the frostedglass
+                theWidth: 350.0,
+                // theHeight is the height of the frostedglass
+                theHeight: 600.0,
+                // theChild is the child of the frostedglass
+
+
+                theChild: Padding(
+                  padding: const EdgeInsets.all(15.0),
+                  child: Column(
+
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children:[
+                        // YAHA PAR NETWORK IMAGE DALEGI
+                        Image.asset('assets/pics/echoesc.png',
+                            width: 400,
+                            height: 100,
+                            fit:BoxFit.fill
+                        ),
+                        SizedBox(height: 10.h),
+
+                        // Text(
+                        //   'About Us                                                                                           ',
+                        //   //'FROSH comprises of various events for the fresher class of Thapar Institute of Engineering and Technology to ease their new surroundings.                                                                  '
+                        //   //  'instagram.com/froshtiet/                                 '
+                        //   //   'linkedin.com/company/frosh-tiet                           '
+                        //   //   'facebook.com/froshtiet         ',
+                        //   style: TextStyle(color: Colors.white54, fontSize: 30.0,fontFamily: 'Roboto',fontWeight: FontWeight.bold,), textAlign: TextAlign.center,)
+
+                        Text('Rotaract is an International Organisation recognized by prestigious institutions. Rotaract is not only limited to social welfare. Instead, domains including vocational skills, soft skills, marketing, and finance skills are all tangible in this club. Each member gets an opportunity to lead and coordinate projects and is awarded resume-worthy certificates for their work. ',
+
+                          style: GoogleFonts.poppins(
+                            textStyle: TextStyle(color: Colors.white, letterSpacing: .5,fontSize: 15.0,),
+                          ),
+                        ),
+                        Text('',
+
+
+                          style: TextStyle(color: Colors.white, fontSize: 20.0,), textAlign: TextAlign.center,),
+
+                        SizedBox(height: 10.h),
+                        Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              // InkWell(
+                              //     onTap: () async{
+                              //       final Uri _url = Uri.parse('https://www.instagram.com/froshtiet/');
+                              //
+                              //       if (!await launchUrl(_url)) {
+                              //         throw 'Could not launch $_url';
+                              //       }
+                              //     },
+                              //   child:
+                              Image.asset('assets/pics/instagram.png',
+                                width: 35,
+                                height: 35,
+                              ),
+                              TextButton(onPressed: ()async{
+                                final Uri _url = Uri.parse('https://instagram.com/rotaractclubtiet?igshid=YmMyMTA2M2Y=');
+
+                                if (!await launchUrl(_url)) {
+                                  throw 'Could not launch $_url';
+                                }
+                              }, child:
+                              Text('/rotoracttiet',
+                                style: GoogleFonts.poppins(
+                                  textStyle: TextStyle(color: Colors.white54, letterSpacing: .5,fontSize: 20.0,),
+                                ),
+                              ),
+                              )]),
+
+                        // Row(
+                        //     mainAxisAlignment: MainAxisAlignment.center,
+                        //     children: [
+                        //       //
+                        //       Image.asset('assets/pics/web.png',
+                        //         width: 35,
+                        //         height: 35,
+                        //       ),
+                        //       TextButton(onPressed: ()async{
+                        //         final Uri _url = Uri.parse('http://echoes-tiet.com/');
+                        //
+                        //         if (!await launchUrl(_url)) {
+                        //           throw 'Could not launch $_url';
+                        //         }
+                        //       }, child:
+                        //       Text('/froshtiet',
+                        //         style: GoogleFonts.poppins(
+                        //           textStyle: TextStyle(color: Colors.white54, letterSpacing: .5,fontSize: 20.0,),
+                        //         ),
+                        //       ),
+                        //       )
+                        //     ]
+                        // ),
+
+                        // Row(
+                        //     mainAxisAlignment: MainAxisAlignment.center,
+                        //     children: [
+                        //       //
+                        //       Image.asset('assets/pics/linkedin.png',
+                        //         width: 35,
+                        //         height: 35,
+                        //       ),
+                        //       TextButton(onPressed: ()async{
+                        //         final Uri _url = Uri.parse('https://www.linkedin.com/company/frosh-tiet');
+                        //
+                        //         if (!await launchUrl(_url)) {
+                        //           throw 'Could not launch $_url';
+                        //         }
+                        //       }, child:
+                        //       Text('/froshtiet',style: TextStyle(color: Colors.white54, fontSize: 25.0,), textAlign: TextAlign.center, ),)]),
+
+                        SizedBox(height: 10.h),
+
+
+
+                        //SizedBox(width: 350.w,),
+                        SizedBox(height: 10.h),
+                        // DiscoverCard3(
+                        //
+                        //   onTap: (){
+                        //     Navigator.push(context, MaterialPageRoute(builder: (context)=>GalleryPage()));
+                        //
+                        //   },
+                        //   title: "Our Team",
+                        //
+                        //   //subtitle: "How to read timetable?",
+                        //   // gradientStartColor: Color(0xffFC67A7),
+                        //   //gradientEndColor: Color(0xffF6815B),
+                        // ),
+
+                      ]
+                  ),
+                )
+            ),
+          ),
+            // Divider(
+            //   color: Colors.white,
+            //   thickness: 3,
+            //
+            //
+            // ),
+            // Row(
+            //   children: [
+                // Row(
+                //     mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                //     children: [
+                //       //
+                //       Image.asset('assets/pics/instagram.png',
+                //         width: 30,
+                //         height: 35,
+                //       ),
+                //       TextButton(onPressed: ()async{
+                //         final Uri _url = Uri.parse('https://www.instagram.com/backslashtiet/');
+                //
+                //         if (!await launchUrl(_url)) {
+                //           throw 'Could not launch $_url';
+                //         }
+                //       }, child:
+                //       Text('/backslashtiet',style: TextStyle(color: Colors.white54, fontSize: 23.0,), textAlign: TextAlign.justify, ),)]),
+
+                // Spacer(),
+
+                //
+                // Row(
+                //     mainAxisAlignment: MainAxisAlignment.end,
+                //     children: [
+                //       //
+                //       Image.asset('assets/pics/web.png',
+                //         width: 30,
+                //         height: 35,
+                //       ),
+                //       TextButton(onPressed: ()async{
+                //         final Uri _url = Uri.parse('http://backslashdb.in/');
+                //
+                //         if (!await launchUrl(_url)) {
+                //           throw 'Could not launch $_url';
+                //         }
+                //       }, child:
+                //       Text('/backslashiet',style: TextStyle(color: Colors.white54, fontSize: 23.0,), textAlign: TextAlign.start, ),)]),
+            //   ],
+            // ),
+          ],
+        ),
+      ),
+    );
+
+  }
+
+  void onSeeAllTapped() {
+  }
+
+  void onSleepMeditationTapped() {
+    Get.to(() => DetailPage(), transition: Transition.rightToLeft);
+  }
+  void onDepressionHealingTapped() {
+  }
+
+  void onSearchIconTapped() {
+  }
+}
