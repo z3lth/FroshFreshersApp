551457bcb0cf60fc11189b6bc6100c7f3c5498f3
shreyas1504 <112496017+shreyas1504@users.noreply.github.com>
2022-09-02 12:03:14 +0530

    Add files via upload

---
+++ lib/boyshostel_page.dart
@@ -0,0 +1,155 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:get/get.dart';
+
+import 'package:meditation/detail_page.dart';
+import 'package:meditation/girlshostel_page.dart';
+import 'package:meditation/hostela_page.dart';
+import 'package:meditation/hostelc_page.dart';
+import 'package:meditation/hostelh_page.dart';
+import 'package:meditation/hostelj_page.dart';
+import 'package:meditation/hostelk_page.dart';
+import 'package:meditation/hostell_page.dart';
+import 'package:meditation/hostelm_page.dart';
+import 'package:meditation/hostelo_page.dart';
+import 'package:meditation/widgets/category_boxes.dart';
+import 'package:meditation/icons.dart';
+import 'package:meditation/widgets/discover_card.dart';
+import 'package:meditation/widgets/discover_card1.dart';
+import 'package:meditation/widgets/discover_small_card.dart';
+import 'package:meditation/widgets/svg_asset.dart';
+//import 'package:url_launcher/url_launcher.dart';
+
+class BoyshostelPage extends StatefulWidget {
+  const BoyshostelPage({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  State<BoyshostelPage> createState() => _BoyshostelPageState();
+}
+
+class _BoyshostelPageState extends State<BoyshostelPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color(0xff121421),
+      body: SafeArea(
+        child: ListView(
+          physics: BouncingScrollPhysics(),
+          children: [
+            Padding(
+              padding: EdgeInsets.only(
+                left: 70.w,
+                right: 50.w,
+                top: 36.h,
+              ),
+
+            ),
+            SizedBox(
+              height: 16.h,
+            ),
+            SizedBox(
+              height: 121.w,
+
+              child: ListView(
+                physics: BouncingScrollPhysics(),
+                scrollDirection: Axis.horizontal,
+                children: [
+                  SizedBox(width: 35.w,),
+                  InkWell(
+                    child: DiscoverCard1(
+
+                      onTap:() {
+                        Navigator.push(context, MaterialPageRoute(builder:(context)=>HostelaPage()));
+                      },
+                      title: "Hostel A",
+                      subtitle: "Established 1980",
+                      gradientStartColor: Color(0xff13DEA0),
+                      gradientEndColor: Color(0xff06B782),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            SizedBox(height: 10.h),
+
+            SizedBox(
+              height: 121.w,
+
+              child: ListView(
+                physics: BouncingScrollPhysics(),
+                scrollDirection: Axis.horizontal,
+                children: [
+                  SizedBox(width: 35.w,),
+                  InkWell(
+                    child: DiscoverCard1(
+
+                      onTap:() {
+                        Navigator.push(context, MaterialPageRoute(builder:(context)=>HostelhPage()));
+                      },
+                      title: "Hostel H",
+                      subtitle: "Established 1980",
+                      gradientStartColor: Color(0xffFC67A7),
+                      gradientEndColor: Color(0xffF6815B),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            SizedBox(height: 10.h),
+
+            SizedBox(
+              height: 121.w,
+
+              child: ListView(
+                physics: BouncingScrollPhysics(),
+                scrollDirection: Axis.horizontal,
+                children: [
+                  SizedBox(width: 35.w,),
+                  InkWell(
+                    child: DiscoverCard1(
+
+                      onTap:() {
+                        Navigator.push(context, MaterialPageRoute(builder:(context)=>HosteljPage()));
+                      },
+                      title: "Hostel J",
+                      subtitle: "Established 2006",
+                      gradientStartColor: Color(0xff13DEA0),
+                      gradientEndColor: Color(0xff06B782),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            SizedBox(height: 10.h),
+
+
+
+
+
+
+
+
+
+
+
+          ],
+        ),
+      ),
+    );
+  }
+  void onSeeAllTapped() {
+  }
+
+  void onSleepMeditationTapped() {
+    Get.to(()=> DetailPage(), transition: Transition.rightToLeft);
+  }
+
+  void onDepressionHealingTapped() {
+  }
+
+  void onSearchIconTapped() {
+  }
+}
---
+++ lib/girlshostel_page.dart
@@ -0,0 +1,169 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:get/get.dart';
+import 'package:meditation/detail_page.dart';
+import 'package:meditation/hostela_page.dart';
+import 'package:meditation/hostele_page.dart';
+import 'package:meditation/hostelg_page.dart';
+import 'package:meditation/hosteli_page.dart';
+import 'package:meditation/hostelk_page.dart';
+import 'package:meditation/hosteln_page.dart';
+import 'package:meditation/hostelpg_page.dart';
+import 'package:meditation/hostelq_page.dart';
+import 'package:meditation/widgets/category_boxes.dart';
+import 'package:meditation/icons.dart';
+import 'package:meditation/widgets/discover_card1.dart';
+import 'package:meditation/widgets/discover_small_card.dart';
+import 'package:meditation/widgets/svg_asset.dart';
+//import 'package:url_launcher/url_launcher.dart';
+
+class GirlshostelPage extends StatefulWidget {
+  const GirlshostelPage({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  State<GirlshostelPage> createState() => _GirlshostelPageState();
+}
+
+class _GirlshostelPageState extends State<GirlshostelPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color(0xff121421 ),
+      body: SafeArea(
+        child: ListView(
+          physics: BouncingScrollPhysics(),
+          children: [
+            Padding(
+              padding: EdgeInsets.only(
+                left: 70.w,
+                right: 50.w,
+                top: 36.h,
+              ),
+
+            ),
+            SizedBox(
+              height: 16.h,
+            ),
+
+            SizedBox(
+              height: 121.w,
+
+              child: ListView(
+                physics: BouncingScrollPhysics(),
+                scrollDirection: Axis.horizontal,
+                children: [
+                  SizedBox(width: 35.w,),
+                  InkWell(
+                    child: DiscoverCard1(
+
+                      onTap:() {
+                        Navigator.push(context, MaterialPageRoute(builder:(context)=>HostelePage()));
+                      },
+                      title: "Hostel E",
+                      subtitle: "Established 1980",
+                      gradientStartColor: Color(0xffFC67A7),
+                      gradientEndColor: Color(0xffF6815B),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            SizedBox(height: 10.h),
+
+
+            SizedBox(
+              height: 121.w,
+
+              child: ListView(
+                physics: BouncingScrollPhysics(),
+                scrollDirection: Axis.horizontal,
+                children: [
+                  SizedBox(width: 35.w,),
+                  DiscoverCard1(
+
+                    onTap:() {
+                      Navigator.push(context, MaterialPageRoute(builder:(context)=>HostelgPage()));
+                    },
+                    title: "Hostel G",
+                    subtitle: "Established 1980",
+                    gradientStartColor: Color(0xff13DEA0),
+                    gradientEndColor: Color(0xff06B782),
+                  ),
+                ],
+              ),
+            ),
+            SizedBox(height: 10.h),
+
+            SizedBox(
+              height: 121.w,
+
+              child: ListView(
+                physics: BouncingScrollPhysics(),
+                scrollDirection: Axis.horizontal,
+                children: [
+                  SizedBox(width: 35.w,),
+                  DiscoverCard1(
+
+                    onTap:() {
+                      Navigator.push(context, MaterialPageRoute(builder:(context)=>HosteliPage()));
+                    },
+                    title: "Hostel I",
+                    subtitle: "Established 1980",
+                    gradientStartColor: Color(0xffFC67A7),
+                    gradientEndColor: Color(0xffF6815B),
+                  ),
+                ],
+              ),
+            ),
+            SizedBox(height: 10.h),
+
+            SizedBox(
+              height: 121.w,
+
+              child: ListView(
+                physics: BouncingScrollPhysics(),
+                scrollDirection: Axis.horizontal,
+                children: [
+                  SizedBox(width: 35.w,),
+                  DiscoverCard1(
+
+                    onTap:() {
+                      Navigator.push(context, MaterialPageRoute(builder:(context)=>HostelpgPage()));
+                    },
+                    title: "Hostel PG ",
+                    subtitle: "Established 1980",
+                    gradientStartColor: Color(0xffFFD541),
+                    gradientEndColor: Color(0xffF0B31A),
+                  ),
+                ],
+              ),
+            ),
+            SizedBox(height: 10.h),
+
+
+
+
+
+
+
+          ],
+        ),
+      ),
+    );
+  }
+  void onSeeAllTapped() {
+  }
+
+  void onSleepMeditationTapped() {
+    Get.to(()=> DetailPage(), transition: Transition.rightToLeft);
+  }
+
+  void onDepressionHealingTapped() {
+  }
+
+  void onSearchIconTapped() {
+  }
+}
