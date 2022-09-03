4eb37e94ab7514e0a3b36300cbc72f5f68d76689
abhimanyu-mehta26 <94471929+abhimanyu-mehta26@users.noreply.github.com>
2022-09-03 16:28:01 +0530

    Add files via upload

---
+++ lib/boxes/aahar.dart
@@ -0,0 +1,119 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:meditation/icons.dart';
+import 'package:meditation/widgets/svg_asset.dart';
+
+class Aahar extends StatelessWidget {
+  final String? title;
+  final String? subtitle;
+  final Color? gradientStartColor;
+  final Color? gradientEndColor;
+  final double? height;
+  final double? width;
+  final Widget? vectorBottom;
+  final Widget? vectorTop;
+  final Function? onTap;
+  final String? tag;
+  final Widget? child;
+  const Aahar(
+      {Key? key,
+        this.title,
+        this.subtitle,
+        this.gradientStartColor,
+        this.gradientEndColor,
+        this.height,
+        this.width,
+        this.vectorBottom,
+        this.vectorTop,
+        this.onTap,
+        this.tag,
+        this.child,})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      color: Colors.transparent,
+      child: InkWell(
+        onTap: () => onTap!(),
+        borderRadius: BorderRadius.circular(20),
+        child: Ink(
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(20),
+            image: DecorationImage(
+              // Import image in pubspec.yaml first
+              image: NetworkImage('https://i.imgur.com/E5aRgoV.png'),
+              // BoxFit.cover scales up the image until it completely covers the screen
+              fit: BoxFit.cover,
+            ),
+          ),
+          child: Container(
+            height: 176.w,
+            width: 305.w,
+            child: Stack(
+              children: [
+                vectorBottom ??
+                    ClipRRect(
+                      borderRadius: BorderRadius.circular(26),
+                      child: SvgAsset(
+                          height: 176.w,
+                          width: 305.w,
+                          assetName: AssetName.vectorBottom),
+                    ),
+                vectorTop ??
+                    ClipRRect(
+                      borderRadius: BorderRadius.circular(26),
+                      child: SvgAsset(
+                          height: 176.w,
+                          width: 305.w,
+                          assetName: AssetName.vectorTop),
+                    ),
+                Padding(
+                  padding: EdgeInsets.only(left: 24.w, top: 24.h, bottom: 24.h),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Hero(
+                            tag: tag ?? '',
+                            child: Material(
+                              color: Colors.transparent,
+                              child: Text(
+                                title!,
+                                style: TextStyle(
+                                    fontSize: 25.w,
+                                    fontWeight: FontWeight.bold,
+                                    color: Colors.white),
+                              ),
+                            ),
+                          ),
+                          SizedBox(
+                            height: 5.h,
+                          ),
+                          subtitle != null
+                              ? Text(
+                            subtitle!,
+                            style: TextStyle(
+                                fontSize: 16.w,
+                                fontWeight: FontWeight.w300,
+                                color: Colors.white),
+                          )
+                              : Container(),
+                        ],
+                      ),
+
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
