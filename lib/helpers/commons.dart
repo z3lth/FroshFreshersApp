290da42f3548cc823a618ea5a7d2e1549f5204ed
Prithviraj Burange <97468584+z3lth@users.noreply.github.com>
2022-09-03 21:16:04 +0530

    Delete lib/helpers directory

--- lib/helpers/commons.dart
+++
@@ -1,8 +0,0 @@
-import 'package:mapbox_gl/mapbox_gl.dart';
-
-import '../constants/restaurants.dart';
-
-LatLng getLatLngFromRestaurantData(int index, {required int zoom}) {
-  return LatLng(double.parse(restaurants[index]['coordinates']['latitude']),
-      double.parse(restaurants[index]['coordinates']['longitude']));
-}
