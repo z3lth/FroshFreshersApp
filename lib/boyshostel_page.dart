7a36c486d2c105ad08fd5435762b6a7e6c6b8fd3
abhimanyu-mehta26 <94471929+abhimanyu-mehta26@users.noreply.github.com>
2022-09-04 10:26:52 +0530

    Update boyshostel_page.dart

--- lib/boyshostel_page.dart
+++ lib/boyshostel_page.dart
@@ -2,10 +2,10 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get/get.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 import 'package:meditation/detail_page.dart';
 import 'package:meditation/girlshostel_page.dart';
-import 'package:meditation/hostela_page.dart';
 import 'package:meditation/hostelc_page.dart';
 import 'package:meditation/hostelh_page.dart';
 import 'package:meditation/hostelj_page.dart';
@@ -19,6 +19,8 @@ import 'package:meditation/widgets/discover_card.dart';
 import 'package:meditation/widgets/discover_card1.dart';
 import 'package:meditation/widgets/discover_small_card.dart';
 import 'package:meditation/widgets/svg_asset.dart';
+
+import 'hostela_page.dart';
 //import 'package:url_launcher/url_launcher.dart';
 
 class BoyshostelPage extends StatefulWidget {
@@ -41,11 +43,38 @@ class _BoyshostelPageState extends State<BoyshostelPage> {
           children: [
             Padding(
               padding: EdgeInsets.only(
-                left: 70.w,
+                left: 80.w,
                 right: 50.w,
                 top: 36.h,
               ),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text("Boys Hostels",
+                      textAlign:TextAlign.center,
+                      style: GoogleFonts.poppins(
+                      textStyle: TextStyle(
 
+                          color: Colors.white,
+                          fontSize: 32.w,
+                          fontWeight: FontWeight.bold))),
+                  // InkWell(
+                  //   borderRadius: BorderRadius.circular(360),
+                  //   onTap: onSearchIconTapped,
+                  //   child: Container(
+                  //     height: 35.w,
+                  //     width: 35.w,
+                  //     child: Center(
+                  //       child: SvgAsset(
+                  //         assetName: AssetName.search,
+                  //         height: 24.w,
+                  //         width: 24.w,
+                  //       ),
+                  //     ),
+                  //   ),
+                  // ),
+                ],
+              ),
             ),
             SizedBox(
               height: 16.h,
@@ -65,9 +94,33 @@ class _BoyshostelPageState extends State<BoyshostelPage> {
                         Navigator.push(context, MaterialPageRoute(builder:(context)=>HostelaPage()));
                       },
                       title: "Hostel A",
-                      subtitle: "Established 1980",
-                      gradientStartColor: Color(0xff13DEA0),
-                      gradientEndColor: Color(0xff06B782),
+                      subtitle: " ",
+                      gradientStartColor: Color(0xffFF4B5C),
+                      gradientEndColor: Color(0xffF6815B),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            SizedBox(height: 10.h),
+            SizedBox(
+              height: 121.w,
+
+              child: ListView(
+                physics: BouncingScrollPhysics(),
+                scrollDirection: Axis.horizontal,
+                children: [
+                  SizedBox(width: 35.w,),
+                  InkWell(
+                    child: DiscoverCard1(
+
+                      onTap:() {
+                        Navigator.push(context, MaterialPageRoute(builder:(context)=>HostelcPage()));
+                      },
+                      title: "Hostel B",
+                      subtitle: " ",
+                      gradientStartColor: Color(0xff056674),
+                      gradientEndColor: Color(0xff66BFBF),
                     ),
                   ),
                 ],
@@ -87,11 +140,112 @@ class _BoyshostelPageState extends State<BoyshostelPage> {
                     child: DiscoverCard1(
 
                       onTap:() {
+                        Navigator.push(context, MaterialPageRoute(builder:(context)=>HostelcPage()));
+                      },
+                      title: "Hostel C",
+                      subtitle: " ",
+                      gradientStartColor: Color(0xffFF4B5C),
+                      gradientEndColor: Color(0xffF6815B),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            SizedBox(height: 10.h),
+            SizedBox(
+              height: 121.w,
+
+              child: ListView(
+                physics: BouncingScrollPhysics(),
+                scrollDirection: Axis.horizontal,
+                children: [
+                  SizedBox(width: 35.w,),
+                  InkWell(
+                    child: DiscoverCard1(
+
+                      onTap:() {
                         Navigator.push(context, MaterialPageRoute(builder:(context)=>HostelhPage()));
                       },
                       title: "Hostel H",
-                      subtitle: "Established 1980",
-                      gradientStartColor: Color(0xffFC67A7),
+                      subtitle: " ",
+                      gradientStartColor: Color(0xff056674),
+                      gradientEndColor: Color(0xff66BFBF),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            SizedBox(height: 10.h),
+
+
+            SizedBox(
+              height: 121.w,
+
+              child: ListView(
+                physics: BouncingScrollPhysics(),
+                scrollDirection: Axis.horizontal,
+                children: [
+                  SizedBox(width: 35.w,),
+                  InkWell(
+                    child: DiscoverCard1(
+
+                      onTap:() {
+                        Navigator.push(context, MaterialPageRoute(builder:(context)=>HosteljPage()));
+                      },
+                      title: "Hostel J",
+                      subtitle: " ",
+                      gradientStartColor: Color(0xffFF4B5C),
+                      gradientEndColor: Color(0xffF6815B),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            SizedBox(height: 10.h),
+
+
+            SizedBox(
+              height: 121.w,
+
+              child: ListView(
+                physics: BouncingScrollPhysics(),
+                scrollDirection: Axis.horizontal,
+                children: [
+                  SizedBox(width: 35.w,),
+                  InkWell(
+                    child: DiscoverCard1(
+
+                      onTap:() {
+                        Navigator.push(context, MaterialPageRoute(builder:(context)=>HostelkPage()));
+                      },
+                      title: "Hostel K ",
+                      subtitle: " ",
+                      gradientStartColor: Color(0xff056674),
+                      gradientEndColor: Color(0xff66BFBF),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            SizedBox(height: 10.h),
+
+            SizedBox(
+              height: 121.w,
+
+              child: ListView(
+                physics: BouncingScrollPhysics(),
+                scrollDirection: Axis.horizontal,
+                children: [
+                  SizedBox(width: 35.w,),
+                  InkWell(
+                    child: DiscoverCard1(
+
+                      onTap:() {
+                        Navigator.push(context, MaterialPageRoute(builder:(context)=>HostellPage()));
+                      },
+                      title: "Hostel L",
+                      subtitle: " ",
+                      gradientStartColor: Color(0xffFF4B5C),
                       gradientEndColor: Color(0xffF6815B),
                     ),
                   ),
@@ -109,15 +263,17 @@ class _BoyshostelPageState extends State<BoyshostelPage> {
                 children: [
                   SizedBox(width: 35.w,),
                   InkWell(
-                    child: DiscoverCard1(
+                    child: InkWell(
+                      child: DiscoverCard1(
 
-                      onTap:() {
-                        Navigator.push(context, MaterialPageRoute(builder:(context)=>HosteljPage()));
-                      },
-                      title: "Hostel J",
-                      subtitle: "Established 2006",
-                      gradientStartColor: Color(0xff13DEA0),
-                      gradientEndColor: Color(0xff06B782),
+                        onTap:() {
+                          Navigator.push(context, MaterialPageRoute(builder:(context)=>HostelmPage()));
+                        },
+                        title: "Hostel M",
+                        subtitle: " ",
+                        gradientStartColor: Color(0xff056674),
+                        gradientEndColor: Color(0xff66BFBF),
+                      ),
                     ),
                   ),
                 ],
@@ -125,12 +281,29 @@ class _BoyshostelPageState extends State<BoyshostelPage> {
             ),
             SizedBox(height: 10.h),
 
+            SizedBox(
+              height: 121.w,
 
+              child: ListView(
+                physics: BouncingScrollPhysics(),
+                scrollDirection: Axis.horizontal,
+                children: [
+                  SizedBox(width: 35.w,),
+                  InkWell(
+                    child: DiscoverCard1(
 
-
-
-
-
+                      onTap:() {
+                        Navigator.push(context, MaterialPageRoute(builder:(context)=>HosteloPage()));
+                      },
+                      title: "Hostel O",
+                      subtitle: " ",
+                      gradientStartColor: Color(0xffFF4B5C),
+                      gradientEndColor: Color(0xffF6815B),
+                    ),
+                  ),
+                ],
+              ),
+            ),
 
 
 
