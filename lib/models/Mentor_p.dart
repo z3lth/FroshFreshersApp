b99d3a347094569b126c109fdf0cadd4d4aa5da9
abhimanyu-mehta26 <94471929+abhimanyu-mehta26@users.noreply.github.com>
2022-09-04 11:08:46 +0530

    Update Mentor_p.dart

--- lib/models/Mentor_p.dart
+++ lib/models/Mentor_p.dart
@@ -47,7 +47,7 @@ final List<Models> snk1Models = [
   ),
   Models(
     name: "Abhimanyu",
-    model: " ",
+    model: "M",
     branch: "COE",
     imgAddress: "https://i.imgur.com/q4fJ1cV.jpg",
     modelColor: Color(0xff3F7943),
@@ -59,7 +59,7 @@ final List<Models> snk1Models = [
 final List<Models> snk2Models = [
   Models(
     name: "Abhimanyu",
-    model: "Narayanan",
+    model: "N",
     branch: "ELE",
     imgAddress: "https://i.imgur.com/GjWhyvW.jpg ",
     modelColor: Color(0xffDE0106),
@@ -133,13 +133,13 @@ final List<Models> snk5Models = [
 
 ];final List<Models> snk6Models = [
 
-    Models(
-      name: "Ayesha",
-      model: "Gupta",
-      branch: "COE",
-      imgAddress: "https://i.imgur.com/VqARCJA.jpg",
-      modelColor: Color(0xff3F7943),
-    ),
+  Models(
+    name: "Ayesha",
+    model: "Gupta",
+    branch: "COE",
+    imgAddress: "https://i.imgur.com/VqARCJA.jpg",
+    modelColor: Color(0xff3F7943),
+  ),
   Models(
     name: "Chinmayee",
     model: "Goyal",
@@ -181,13 +181,13 @@ final List<Models> snk5Models = [
   ),
 
 ];final List<Models> snk9Models = [
-  Models(
-    name: "Ikshan",
-    model: "Bhardwaj",
-    branch: "COPC",
-    imgAddress: "https://i.imgur.com/FovYvvY.jpg",
-    modelColor: Color(0xff3F7943),
-  ),
+  // Models(
+  //   name: "Ikshan",
+  //   model: "Bhardwaj",
+  //   branch: "COPC",
+  //   imgAddress: "https://i.imgur.com/FovYvvY.jpg",
+  //   modelColor: Color(0xff3F7943),
+  // ),
   Models(
     name: "Ishank Gupta",
     model: " ",
@@ -195,8 +195,6 @@ final List<Models> snk5Models = [
     imgAddress: "https://i.imgur.com/MdbwCFu.png",
     modelColor: Color(0xffDE0106),
   ),
-
-];final List<Models> snk10Models = [
   Models(
     name: "Ishika Garg",
     model: " ",
@@ -204,6 +202,8 @@ final List<Models> snk5Models = [
     imgAddress: "https://i.imgur.com/O1jlS8k.jpg",
     modelColor: Color(0xff3F7943),
   ),
+];final List<Models> snk10Models = [
+
   Models(
     name: "Ishita Bhatnagar",
     model: " ",
@@ -211,8 +211,6 @@ final List<Models> snk5Models = [
     imgAddress: "https://i.imgur.com/MTrUbfd.png",
     modelColor: Color(0xffDE0106),
   ),
-
-];final List<Models> snk11Models = [
   Models(
     name: "Kashish Jain",
     model: " ",
@@ -220,6 +218,9 @@ final List<Models> snk5Models = [
     imgAddress: "https://i.imgur.com/Z4kGoPD.png",
     modelColor: Color(0xff3F7943),
   ),
+
+];final List<Models> snk11Models = [
+
   Models(
     name: "Kashish Sisodia",
     model: " ",
@@ -227,8 +228,6 @@ final List<Models> snk5Models = [
     imgAddress: "https://i.imgur.com/cPorZCL.png",
     modelColor: Color(0xffDE0106),
   ),
-
-];final List<Models> snk12Models = [
   Models(
     name: "Khushi Singh",
     model: " ",
@@ -236,6 +235,9 @@ final List<Models> snk5Models = [
     imgAddress: " ",
     modelColor: Color(0xff3F7943),
   ),
+
+];final List<Models> snk12Models = [
+
   Models(
     name: "Khushi",
     model: "Talwar ",
@@ -243,8 +245,6 @@ final List<Models> snk5Models = [
     imgAddress: "https://i.imgur.com/wsOe9Wc.jpg",
     modelColor: Color(0xffDE0106),
   ),
-
-];final List<Models> snk13Models = [
   Models(
     name: "Khyati Sharma",
     model: " ",
@@ -252,6 +252,9 @@ final List<Models> snk5Models = [
     imgAddress: "https://i.imgur.com/3DTdV2U.png",
     modelColor: Color(0xff3F7943),
   ),
+
+];final List<Models> snk13Models = [
+
   Models(
     name: "Kushal Gupta",
     model: " ",
@@ -259,8 +262,6 @@ final List<Models> snk5Models = [
     imgAddress: "https://i.imgur.com/qdMQxgo.png",
     modelColor: Color(0xffDE0106),
   ),
-
-];final List<Models> snk14Models = [
   Models(
     name: "Lakshya Gupta",
     model: " ",
@@ -268,6 +269,8 @@ final List<Models> snk5Models = [
     imgAddress: "https://i.imgur.com/3vZVUBK.jpg",
     modelColor: Color(0xff3F7943),
   ),
+];final List<Models> snk14Models = [
+
   Models(
     name: "Manan Katnoria",
     model: " ",
@@ -275,8 +278,6 @@ final List<Models> snk5Models = [
     imgAddress: "https://i.imgur.com/pFWIgEL.jpg",
     modelColor: Color(0xffDE0106),
   ),
-
-];final List<Models> snk15Models = [
   Models(
     name: "Monishka ",
     model: " ",
@@ -284,6 +285,9 @@ final List<Models> snk5Models = [
     imgAddress: "https://i.imgur.com/h7qD8qa.jpg",
     modelColor: Color(0xff3F7943),
   ),
+
+];final List<Models> snk15Models = [
+
   Models(
     name: "Mridul Saini",
     model: " ",
@@ -291,9 +295,6 @@ final List<Models> snk5Models = [
     imgAddress: "https://i.imgur.com/ukxAqUr.jpg",
     modelColor: Color(0xffDE0106),
   ),
-];
-
-final List<Models> snk16Models = [
   Models(
     name: "Muskan",
     model: "Bhatia",
@@ -301,6 +302,10 @@ final List<Models> snk16Models = [
     imgAddress: "https://i.imgur.com/ToIa6JX.jpg",
     modelColor: Color(0xffDE0106),
   ),
+];
+
+final List<Models> snk16Models = [
+
   Models(
     name: "Paras",
     model: "Sharma",
@@ -308,8 +313,6 @@ final List<Models> snk16Models = [
     imgAddress: "https://i.imgur.com/rLIwOBg.png",
     modelColor: Color(0xff3F7943),
   ),
-];
-final List<Models> snk17Models = [
   Models(
     name: "Pratinav",
     model: "Batra",
@@ -317,6 +320,9 @@ final List<Models> snk17Models = [
     imgAddress: "https://i.imgur.com/XIWFlt4.jpg",
     modelColor: Color(0xffDE0106),
   ),
+];
+final List<Models> snk17Models = [
+
   Models(
     name: "Prithviraj",
     model: "Burange",
@@ -324,7 +330,6 @@ final List<Models> snk17Models = [
     imgAddress: "https://i.imgur.com/oRAdFWF.png",
     modelColor: Color(0xff3F7943),
   ),
-];final List<Models> snk18Models = [
   Models(
     name: "Rachit Bhagat",
     model: " ",
@@ -332,6 +337,8 @@ final List<Models> snk17Models = [
     imgAddress: "https://i.imgur.com/HsgI8to.jpg",
     modelColor: Color(0xffDE0106),
   ),
+];final List<Models> snk18Models = [
+
   Models(
     name: "Raushni",
     model: "Plaha",
@@ -339,8 +346,6 @@ final List<Models> snk17Models = [
     imgAddress: "https://i.imgur.com/H9Cjwrg.jpg",
     modelColor: Color(0xff3F7943),
   ),
-];
-final List<Models> snk19Models = [
   Models(
     name: "Rishabh",
     model: "Ahuja",
@@ -348,6 +353,9 @@ final List<Models> snk19Models = [
     imgAddress: "https://i.imgur.com/jx7x9gF.jpg",
     modelColor: Color(0xffDE0106),
   ),
+];
+final List<Models> snk19Models = [
+
   Models(
     name: "Samayak K",
     model: "Mishra",
@@ -355,8 +363,6 @@ final List<Models> snk19Models = [
     imgAddress: "https://i.imgur.com/40w6Uq3.jpg",
     modelColor: Color(0xff3F7943),
   ),
-];
-final List<Models> snk20Models = [
   Models(
     name: "Sanya Singh",
     model: " ",
@@ -364,6 +370,9 @@ final List<Models> snk20Models = [
     imgAddress: "https://i.imgur.com/OBy8KUi.jpg",
     modelColor: Color(0xffDE0106),
   ),
+];
+final List<Models> snk20Models = [
+
   Models(
     name: "Saumay",
     model: "Ahuja",
@@ -371,8 +380,6 @@ final List<Models> snk20Models = [
     imgAddress: "https://i.imgur.com/YfUgqx3.png",
     modelColor: Color(0xff3F7943),
   ),
-];
-final List<Models> snk21Models = [
   Models(
     name: "Shreyas",
     model: " ",
@@ -380,14 +387,17 @@ final List<Models> snk21Models = [
     imgAddress: "https://i.imgur.com/y8cXuMi.jpg ",
     modelColor: Color(0xffDE0106),
   ),
+];
+final List<Models> snk21Models = [
+
   Models(
     name: "Sitanshu",
     model: "Kaloti",
     branch: "MEE",
     imgAddress: "https://i.imgur.com/GJfX84l.png",
     modelColor: Color(0xff3F7943),
+
   ),
-];final List<Models> snk22Models = [
   Models(
     name: "Swastik",
     model: "Raghav",
@@ -395,14 +405,7 @@ final List<Models> snk21Models = [
     imgAddress: "https://i.imgur.com/CqZGmAs.png",
     modelColor: Color(0xffDE0106),
   ),
-  Models(
-    name: "Tanish",
-    model: "Kharkwal",
-    branch: "ENC",
-    imgAddress: " ",
-    modelColor: Color(0xff3F7943),
-  ),
-];final List<Models> snk23Models = [
+];final List<Models> snk22Models = [
   Models(
     name: "Tushyag",
     model: "Shubham",
@@ -417,7 +420,8 @@ final List<Models> snk21Models = [
     imgAddress: "https://i.imgur.com/35J0224.jpg",
     modelColor: Color(0xff3F7943),
   ),
-];final List<Models> snk24Models = [
+
+];final List<Models> snk23Models = [
   Models(
     name: "Vinayak",
     model: "Abrol",
@@ -432,7 +436,7 @@ final List<Models> snk21Models = [
     imgAddress: "https://i.imgur.com/owAzpHd.jpg",
     modelColor: Color(0xff3F7943),
   ),
-];final List<Models> snk25Models = [
+];final List<Models> snk24Models = [
   Models(
     name: "Vivin",
     model: "Vadhera",
@@ -448,4 +452,3 @@ final List<Models> snk21Models = [
     modelColor: Color(0xff3F7943),
   ),
 ];
-
