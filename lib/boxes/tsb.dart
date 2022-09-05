976f2f4726699dafa4d57c88131e93109f4f6b43
abhimanyu-mehta26 <94471929+abhimanyu-mehta26@users.noreply.github.com>
2022-09-05 18:28:35 +0530

    Update tsb.dart

--- lib/boxes/tsb.dart
+++ lib/boxes/tsb.dart
@@ -58,7 +58,7 @@ class TSB extends StatelessWidget {
         decoration: BoxDecoration(
           borderRadius: BorderRadius.circular(20),
           image: DecorationImage(
-            image: NetworkImage('https://i.imgur.com/U39IvZf.gifv'),
+            image: AssetImage('assets/images/tech.gif'),
                 fit: BoxFit.cover
           )
         ),
